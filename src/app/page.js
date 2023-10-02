183853196e07cbfe501368efbe4fa0f730bd4f4b
Joy <mmehedihasanjoyv@gmail.com>
2023-10-02 17:44:23 +0600

    created the user and customer sql data

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h3 className='text-4xl font-bold text-center'>Hello World, Home page</h3>
+    </div>
+  );
+};
+
+export default page;
